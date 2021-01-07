752bee40966628882a5f5979ffc6c0127cde70b3
Alejandro483 <estesquefawer@gmail.com>
2021-01-07 14:57:24 -0600

    "created HTML and Style sheet"

---
+++ script.js
@@ -0,0 +1,9 @@
+import Deck from './deck.js'
+
+const computerCardSlot = document.querySelector('.computer-card-slot')
+
+const deck = new Deck()
+deck.shuffle()
+console.log(deck)
+
+computerCardSlot.appendChild(deck.cards[0].getHTML())
