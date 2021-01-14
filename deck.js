6772aab9bab2ba9fb1cc50ad47f678f37b218af8
Alejandro483 <estesquefawer@gmail.com>
2021-01-14 14:26:53 -0600

    "finished script and deck.js files"

--- deck.js
+++ deck.js
@@ -1,7 +1,6 @@
 const SUITS = ["♥", "♦", "♠", "♣"]
 const VALUES =[
     "A",
-    "1",
     "2",
     "3",
     "4",
@@ -23,6 +22,14 @@ export default class Deck {
 
     get numberOfCards() {
         return this.cards.length
+    }
+
+    pop() {
+        return this.cards.shift()
+    }
+
+    push(card) {
+         this.cards.push(card)
     }
 
     shuffle() {
